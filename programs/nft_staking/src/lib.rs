58ffaf9988a378448ce1f379eab09b1eb425b35a
Ayush <ayushagarwal.dev@gmail.com>
2024-10-30 20:22:42 +0530

    add initialize-user instruction

--- programs/nft_staking/src/lib.rs
+++ programs/nft_staking/src/lib.rs
@@ -8,12 +8,4 @@ mod instructions;
 #[program]
 pub mod nft_staking {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
